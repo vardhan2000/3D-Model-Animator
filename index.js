64b9f69b8da07b4abffb52e99edbf2fc48eb05d7
vardhan2000 <vardhan.av2000@gmail.com>
2022-03-22 12:09:53 +0530

    imported .obj shape and corrected eventListener

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
-import { Scene, Cube, WebGLRenderer, Shader, Camera } from './lib/threeD.js';
+import { Scene, Cube, WebGLRenderer, Shader, Camera, Shape } from './lib/threeD.js';
 import {vertexShaderSrc} from './shaders/vertex.js';
 import {fragmentShaderSrc} from './shaders/fragment.js';
 import * as dat from 'https://cdn.skypack.dev/dat.gui';
 import { vec3, mat4 } from 'https://cdn.skypack.dev/gl-matrix';
+import objLoader from 'https://cdn.skypack.dev/webgl-obj-loader';
 
 let camera = new Camera();
 
 const scene = new Scene(camera);
 
-const cube = new Cube(0,0);
+async function meshObject(filePath){
+	const response = await fetch(filePath);
+	const text = await response.text();
+	return new objLoader.Mesh(text);
+}
 
-scene.add(cube);
+
+// const cube = new Cube(0,0);
+
+let mesh_AxisZ = await meshObject('./arrowZ.obj');
+// console.log(mesh_AxisZ);
+let axisZ = new Shape(mesh_AxisZ,[1,0,0,1]);
+
+scene.add(axisZ);
 
 const renderer = new WebGLRenderer();
 renderer.setSize( 600, 600 );
@@ -52,93 +64,86 @@ let items1 = new Array(3);
 if(mode==0){
 	items0[0] = gui0.add(transformSettings, 'rotateX', -Math.PI, Math.PI).step(0.01).onChange(function ()
 	{
-		cube.transform.rotationAngle_X = transformSettings.rotateX;
+		axisZ.transform.rotationAngle_X = transformSettings.rotateX;
 	});
 
 	items0[1] = gui0.add(transformSettings, 'rotateY', -Math.PI, Math.PI).step(0.01).onChange(function ()
 	{
-		cube.transform.rotationAngle_Y = transformSettings.rotateY;
+		axisZ.transform.rotationAngle_Y = transformSettings.rotateY;
 	});
 
 	items0[2] = gui0.add(transformSettings, 'rotateZ', -Math.PI, Math.PI).step(0.01).onChange(function ()
 	{
-		cube.transform.rotationAngle_Z = transformSettings.rotateZ;
+		axisZ.transform.rotationAngle_Z = transformSettings.rotateZ;
 	});
 }
 
 
-window.onload = () => {
-	renderer.getDomElement().addEventListener('click', (event) => {
-    });
 
-	window.addEventListener('keydown', function (event) {
-        console.log("Key pressed = ", event.key);
+document.addEventListener('keydown', function (event) {
+	console.log("Key pressed = ", event.key);
 
-		if(event.key=="m"){
-			mode = (mode+=1)%2
-			console.log("mode = ", mode)
-			if(mode==0){
-				console.log("top view")
-				for(let i=0; i<3; i++){
-					gui1.remove(items1[i]);
-				}
+	if(event.key=="m"){
+		mode = (mode+=1)%2
+		console.log("mode = ", mode)
+		if(mode==0){
+			console.log("top view")
+			for(let i=0; i<3; i++){
+				gui1.remove(items1[i]);
+			}
 
-				items0[0] = gui0.add(transformSettings, 'rotateX', -Math.PI, Math.PI).step(0.01).onChange(function ()
-				{
-					cube.transform.rotationAngle_X = transformSettings.rotateX;
-				});
+			items0[0] = gui0.add(transformSettings, 'rotateX', -Math.PI, Math.PI).step(0.01).onChange(function ()
+			{
+				axisZ.transform.rotationAngle_X = transformSettings.rotateX;
+			});
 
-				items0[1] = gui0.add(transformSettings, 'rotateY', -Math.PI, Math.PI).step(0.01).onChange(function ()
-				{
-					cube.transform.rotationAngle_Y = transformSettings.rotateY;
-				});
+			items0[1] = gui0.add(transformSettings, 'rotateY', -Math.PI, Math.PI).step(0.01).onChange(function ()
+			{
+				axisZ.transform.rotationAngle_Y = transformSettings.rotateY;
+			});
 
-				items0[2] = gui0.add(transformSettings, 'rotateZ', -Math.PI, Math.PI).step(0.01).onChange(function ()
-				{
-					cube.transform.rotationAngle_Z = transformSettings.rotateZ;
-				});
+			items0[2] = gui0.add(transformSettings, 'rotateZ', -Math.PI, Math.PI).step(0.01).onChange(function ()
+			{
+				axisZ.transform.rotationAngle_Z = transformSettings.rotateZ;
+			});
 
-				camera.transform.eye = [0,0,6];
+			camera.transform.eye = [0,0,6];
 
-			} else {
-				console.log("3D view")
-				for(let i=0; i<3; i++){
-					gui0.remove(items0[i]);
-				}
+		} else {
+			console.log("3D view")
+			for(let i=0; i<3; i++){
+				gui0.remove(items0[i]);
+			}
 
-				items1[0] = gui1.add(cameraSettings, 'rotateX', 1, Math.PI).step(0.01).onChange(function ()
-				{
-					camera.transform.rotationAngle_X = cameraSettings.rotateX * 0.05;
-					camera.transform.updateViewTransformMatrix();
-				});
+			items1[0] = gui1.add(cameraSettings, 'rotateX', 1, Math.PI).step(0.01).onChange(function ()
+			{
+				camera.transform.rotationAngle_X = cameraSettings.rotateX * 0.05;
+				camera.transform.updateViewTransformMatrix();
+			});
 
-				items1[1] = gui1.add(cameraSettings, 'rotateY', 1, Math.PI).step(0.01).onChange(function ()
-				{
-					camera.transform.rotationAngle_Y = cameraSettings.rotateY * 0.05;
-					camera.transform.updateViewTransformMatrix();
-				});
+			items1[1] = gui1.add(cameraSettings, 'rotateY', 1, Math.PI).step(0.01).onChange(function ()
+			{
+				camera.transform.rotationAngle_Y = cameraSettings.rotateY * 0.05;
+				camera.transform.updateViewTransformMatrix();
+			});
 
-				items1[2] = gui1.add(cameraSettings, 'rotateZ', 1, Math.PI).step(0.01).onChange(function ()
-				{
-					camera.transform.rotationAngle_Z = cameraSettings.rotateZ * 0.05;
-					camera.transform.updateViewTransformMatrix();
-				});
-				camera.transform.eye = [5,-5,5];
-			}		
-		}
+			items1[2] = gui1.add(cameraSettings, 'rotateZ', 1, Math.PI).step(0.01).onChange(function ()
+			{
+				camera.transform.rotationAngle_Z = cameraSettings.rotateZ * 0.05;
+				camera.transform.updateViewTransformMatrix();
+			});
+			camera.transform.eye = [5,-5,5];
+		}		
+	}
 
-		else if(event.key=="+" && mode==0){
-			cube.transform.scale = cube.transform.scale.map(x=>x*1.1)
-		}
+	else if(event.key=="+" && mode==0){
+		axisZ.transform.scale = axisZ.transform.scale.map(x=>x*1.1)
+	}
 
-		else if(event.key=="-" && mode==0){
-			cube.transform.scale = cube.transform.scale.map(x=>x/1.1)
-		}
-	}, true
-	);
-	window.addEventListener
-}
-
+	else if(event.key=="-" && mode==0){
+		axisZ.transform.scale = axisZ.transform.scale.map(x=>x/1.1)
+	}
+}, false );
 
 renderer.setAnimationLoop( animation );
 
